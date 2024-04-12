22b37582033af5325667a3a024f5eadc5f1f21f5
Eoghan O'Keefe <hogchops@pop-os.localdomain>
2024-04-12 09:08:04 -0400

    update

--- vCycleStencil.c
+++ vCycleStencil.c
@@ -7,7 +7,7 @@
 double stencilValue(int i, int j, int n, double sigma, double h) {
     if(i == j) { 
         return 2.0/pow(h,2) + sigma;
-    } else if(i == -1 || j == -1 || i == n || j == n) {
+    } else if(i == -1 || j == -1 || i == n-1 || j == n-1) {
         return 0;
     } else if(i == j + 1 || j == i + 1) {
         return -1.0/pow(h,2);
@@ -67,7 +67,6 @@ void vCycle(double* u, double* f, int n, int nu1, int nu2, double sigma, double 
     double residual2h[n/2-1];
     double error2h[n/2-1];
     double errorh[n-1];
-
     if(n != 2) {
         gaussSeidel(u, f, n, nu1, sigma, h);
 
@@ -79,8 +78,6 @@ void vCycle(double* u, double* f, int n, int nu1, int nu2, double sigma, double 
             residual[i] = f[i] - sum;
         }
 
-        printf("Grid level n=%d, residual norm: %.16e\n", n, norm(residual, n-1)*pow(h,0.5));
-
         restriction(residual, n, residual2h);
 
         for(i = 0; i < n/2-1; i++) {
@@ -88,6 +85,52 @@ void vCycle(double* u, double* f, int n, int nu1, int nu2, double sigma, double 
         }
 
         vCycle(error2h, residual2h, n/2, nu1, nu2, sigma, 2*h);
+        
+        prolongation(error2h, n, errorh);
+
+        for(i = 0; i < n-1; i++) {
+            u[i] = u[i] + errorh[i];
+        }
+
+        gaussSeidel(u, f, n, nu2, sigma, h);
+    } else {
+        u[0] = f[0]/(2+sigma*pow(h,2))*pow(h,2);
+        //printf("Coarsest grid level, norm of u: %.16e\n", norm(u, n-1)*pow(h,0.5));
+    }
+}     
+
+void muCycle(int mu, double* u, double* f, int n, int nu1, int nu2, double sigma, double h) {
+    int i = 0;
+    int j = 0;
+    double sum = 0.0;
+    double residual[n-1];
+    double residual2h[n/2-1];
+    double errorh[n-1];
+    double error2h[n/2-1];
+    if(n != 2) {
+        gaussSeidel(u, f, n, nu1, sigma, h);
+
+        for(i = 0; i < n-1; i++) {
+            sum = 0;
+            for(j = i - 1; j < i + 2; j++) {
+                sum = sum + stencilValue(i, j, n, sigma, h) * u[j];
+            }
+            residual[i] = f[i] - sum;
+        }
+
+        restriction(residual, n, residual2h);
+
+        for(i = 0; i < n/2-1; i++) {
+            error2h[i] = 0;
+        }
+
+        if(n != 4) {
+            for(i = 0; i < mu; i++) {
+                muCycle(mu, error2h, residual2h, n/2, nu1, nu2, sigma, 2*h);
+            }
+        } else {
+            muCycle(mu, error2h, residual2h, n/2, nu1, nu2, sigma, 2*h);
+        }
 
         prolongation(error2h, n, errorh);
 
@@ -95,13 +138,26 @@ void vCycle(double* u, double* f, int n, int nu1, int nu2, double sigma, double 
             u[i] = u[i] + errorh[i];
         }
 
-        printf("Grid level n=%d, error norm: %.16f\n", n, norm(errorh, n-1)*pow(h,0.5));
-
         gaussSeidel(u, f, n, nu2, sigma, h);
     } else {
-        u[0] = f[0]/(2+sigma*pow(h,2));
+        u[0] = f[0]/(2+sigma*pow(h,2))*pow(h,2);
     }
-}     
+}
+
+void FMG(double* u, double* f, int n, int nu1, int nu2, double sigma, double h) {
+    double f2h[n/2-1];
+    double u2h[n/2-1];
+    int i = 0;
+    if(n != 2) {
+        restriction(f, n, f2h);
+        for(i = 0; i < n/2-1; i++) {
+            u2h[i] = 0;
+        }
+        FMG(u2h, f2h, n, nu1, nu2, sigma, h);
+        prolongation(u2h, n, u);
+    }
+    vCycle(u, f, n, nu1, nu2, sigma, h);
+}
 
 int simulate(int k, int nu1, int nu2, double tolerance, FILE* fptr) {
 
@@ -128,10 +184,10 @@ int simulate(int k, int nu1, int nu2, double tolerance, FILE* fptr) {
     i = 0;
     
     while(i == 0 || residualNorm > tolerance) {
-    //for(i = 0; i < 20; i++) {
-        printf("\nIteration %d\n", i+1);
-        vCycle(u, f, n, nu1, nu2, sigma, h);
-        /*
+        //vCycle(u, f, n, nu1, nu2, sigma, h);
+        //muCycle(2, u, f, n, nu1, nu2, sigma, h);
+        FMG(u, f, n, nu1, nu2, sigma, );
+
         // ======= Residual ========
         for(a = 0; a < n-1; a++) {
             sum = 0.0;
@@ -140,21 +196,24 @@ int simulate(int k, int nu1, int nu2, double tolerance, FILE* fptr) {
             }
             residual[a] = f[a] - sum; // r = f - Au
         }
-        residualNorm = norm(residual, n-1)/norm(f, n-1); // ||r||/||f||
+        double fNorm = norm(f, n-1);
+        if(fNorm != 0) {
+            residualNorm = norm(residual, n-1)/norm(f, n-1); // ||r||/||f||
+        } else {
+            residualNorm = norm(residual, n-1);
+        }
+        //printf("After iteration %d, norm of the residual is %.16e\n", i+1, residualNorm);
         // ======= Residual ========
-        */
-
-        residualNorm = norm(u, n-1) * pow(h, 0.5); // Discrete l2 norm
 
         i = i+1;
-        /*
         // ==== Write output to file ====
         fprintf(fptr, "%d,", i);
         for(j = 0; j < n-2; j++) {
             fprintf(fptr, "%.10f,", u[j]);
         }
         fprintf(fptr, "%.10f\n", u[n-2]);
-        */
+
+        printf("\nLOOP\n");
     }
 
     return i; // Return number of iterations
@@ -174,8 +233,8 @@ int main(int argc, char **argv) {
         tolerance = pow(10, -1*atof(argv[3]));
     }
 
-    int k = 3;
-    for(k = 3; k <= 8; k++) {
+    int k = 4;
+    for(k = 4; k <= 4; k++) {
         printf("k = %d -> Num iterations: %d\n", k, simulate(k, nu1, nu2, tolerance, fptr));
     }
     return 0;
